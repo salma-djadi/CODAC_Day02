85a16155335aeaea477d696090a1d7f7aa655f2e
salma-djadi <salma.djadi@epitech.eu>
2021-09-30 09:33:18 +0200

    the second commit

--- ex_12/my_push.sh
+++ ex_12/my_push.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-git add .  
-#read -p "Commit description: " desc  
-git commit -m "$1"  
-git push
+
+if [ $# -eq 0 ]
+then
+    echo "No commit message, no add and no push"
+else 
+   if [ $# -eq 1 ] 
+        then
+        git add .  
+        #read -p "Commit description: " desc  
+        git commit -m "$1"  
+        git push
+    else 
+        git add .  
+        #read -p "Commit description: " desc  
+        git commit -m "$1" 
+        git push
+    fi
+fi    
